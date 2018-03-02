8aeb6e0b38c26a8daf1595e4c36fdc34df6bebaf
Mike Manh <github.123@vteklabs.com>
2018-03-01 19:55:56 -0500

    got it responding to external messaging, from footpedal->ableton->measure_provider->into here

--- src/core.py
+++ src/core.py
@@ -4,6 +4,13 @@ from ..constants import PORT, HOST
 import zmq
 from control.constants import PORT
 
+import sys
+import os
+
+
+def is_only_num_and_spaces(number_sequence):
+    return all(var.isdigit() for var in number_sequence.split())
+
 def listen(name, callable):
 
     context = zmq.Context.instance()
@@ -14,14 +21,19 @@ def listen(name, callable):
 
     def listener(message):
         data = message.decode('ascii')
-        callable([int(d) for d in data.split()])
+        if is_only_num_and_spaces( data ):
+            callable([int(d) for d in data.split()])
+        else:
+            callable( data )
     
     print (callable, "is listening")
-    try:
-        while True:
-        
-            message = sock.recv()
-            listener(message)
-    except Exception as e:
-        print (e)
-        exit()
+    #try:
+    while True:
+    
+        message = sock.recv()
+        listener(message)
+    # except Exception as e:
+    #     exc_type, exc_obj, exc_tb = sys.exc_info()
+    #     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    #     print(exc_type, fname, exc_tb.tb_lineno)
+    #     exit()
